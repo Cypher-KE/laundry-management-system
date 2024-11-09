7d99d0c5aa291d21bc930ece948fcd7323eef644
CypherKE <biodehood@gmail.com>
2024-11-09 19:33:37 +0300

    Initial commit

---
+++ lsmsApp/templates/program.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            Sum Of Three Integers
+        </title>
+    </head>
+    <body>
+        <?php
+            $num1 = 5;
+            $num2 = 8;
+            $num3 = 7;
+            // Using Echo Statement to display the output.
+            echo "The sum of three integers is: ";
+            echo $num1 + $num2 + $num3;
+        ?>
+    </body>
+</html>
